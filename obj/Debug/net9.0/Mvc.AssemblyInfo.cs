cf8594670e6519df85a80842a8755e3527258a8c
olemartin25sfj <olemartin25sfj@gmail.com>
2025-03-21 11:26:22 +0100

    Cleanup

--- obj/Debug/net9.0/Mvc.AssemblyInfo.cs
+++ obj/Debug/net9.0/Mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afe59ab96279d4de6ed2cddca09566a2eeb81176")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
